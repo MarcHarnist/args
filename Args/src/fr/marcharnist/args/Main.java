d436146e6ef808584e0e7ac6f948be0c47127a99
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2020-01-03 16:48:37 +0100

    Initialize

---
+++ Args/src/fr/marcharnist/args/Main.java
@@ -0,0 +1,25 @@
+package fr.marcharnist.args;
+
+/**
+ * @version 1.0
+ * @author GCKEVL - Marc Harnist
+ *
+ * Exercices de veille en Java
+ * 
+ * Héritages
+ * Instanciation d'une classe
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] argumentsPersonnalises) {
+		
+		//Petit test d'affichage des arguments
+		for(String arg : argumentsPersonnalises) {
+			System.out.print(arg + " ");
+		}
+		System.out.println();
+
+	}
+}
